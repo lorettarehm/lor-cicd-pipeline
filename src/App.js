9b9145085e837081f5666dfaa4d88282c170bf11
LR.btcmp <loretta.rehm@gmail.com>
2023-02-09 20:09:14 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import CharacterGallery from './components/CharacterGallery'
+
+function App() {
+
+  return (
+    <div data-test="component-app">
+      <CharacterGallery />
+    </div>
+  );
+}
+
+export default App;
